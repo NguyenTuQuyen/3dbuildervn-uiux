7785742647c24b36ec5f0841abcbdfa2e15278d0
Nguyen Quyen <cuonbinhthuan@gmail.com>
2019-08-19 17:35:15 +0700

    add boostrap

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+
 class Home extends Component {
     render(){
+        let productList = this.props.products.map(item => {
+            return (
+                
+                <div className="product-item card-item col-xs-3 col-sm-4 col-md-3 col-lg-3">
+                    <img className="product-image" src={item.img} alt={item.title} />
+                    <span className="product-title">{item.title}</span>
+                    <span className="product-desc">{item.desc}</span>
+                    <span className="product-price">{item.price}</span>
+                    <button to="/" className="btn btn-primary"><FontAwesomeIcon icon={faPlus} />Add</button>
+                </div>
+            )
+        })
         return(
-            <div className="container" >
+            <div className="home-container" >
                 <h3>Home</h3>
+                <div class="product-list ">
+                    {productList}
+                </div>
             </div>
         )
     }
 }
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+export default connect(mapStateToProps)(Home)
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './app';
+import $ from 'jquery';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import cartReducer from './components/reducers/productsReducer'
 import { createStore } from 'redux'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(cartReducer) 
+const store = createStore(cartReducer)
 
-ReactDOM.render(
-<Provider store={store}><App /></Provider>, document.getElementById('root')
-);
+ReactDOM.render(<Provider store={store} > < App /> </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
